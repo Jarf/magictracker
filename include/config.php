e2a2ee31e1c11465d319eb17f9949628038e635e
Martin Wadeson <martin.wadeson@ccsw.co.uk>
2024-06-03 21:19:19 +0100

    Removed bootstrap, added parchment styling, added random quote fetcher

--- include/config.php
+++ include/config.php
@@ -4,6 +4,7 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'magic');
 define('DB_USER', 'magic');
 define('DB_PASS', 'g8*LDX){uwy(4WB5F2U#NQ');
+define('DB_TYPE', 'mysql');
 
 // File Paths
 define('DIR_ROOT', dirname(__DIR__) . '/');
