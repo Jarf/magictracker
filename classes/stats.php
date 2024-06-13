b229218e451ab149eca5a5ed39e7dc73f704780e
Martin Wadeson <martin.wadeson@ccsw.co.uk>
2024-06-13 21:15:09 +0100

    Added stats template/codepage, amended CSS/JS delivery

---
+++ classes/stats.php
@@ -0,0 +1,11 @@
+<?php
+class Stats{
+	public int $gameId;
+	public int $seasonId;
+	public int $playerId;
+
+	public function __construct(int $seasonId = null, int $gameId = null, int $playerId = null){
+		
+	}
+}
+?>
